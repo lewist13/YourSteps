43afa453b0032ad4e05b6b1b100909364fce0a8a
lewist13 <lewistpro@gmail.com>
2023-05-30 19:39:00 -0400

    Initial Commit

---
+++ YourSteps/YourStepsApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourStepsApp.swift
+//  YourSteps
+//
+//  Created by TaeVon Lewis on 5/30/23.
+//
+
+import SwiftUI
+
+@main
+struct YourStepsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
