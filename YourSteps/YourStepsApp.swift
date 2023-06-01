dbea925c994dc5873076a67aa9049c0f2f055407
lewist13 <lewistpro@gmail.com>
2023-06-01 18:18:45 -0400

    updated the main file to accommodate the home screen argument change

--- YourSteps/YourStepsApp.swift
+++ YourSteps/YourStepsApp.swift
@@ -11,7 +11,13 @@ import SwiftUI
 struct YourStepsApp: App {
     var body: some Scene {
         WindowGroup {
-            Home()
+            Home(thresholdsAndLabels: [
+                ((0, 4999), "Sedentary"),
+                ((5000, 7499), "Low Active"),
+                ((7500, 9999), "Somewhat Active"),
+                ((10000, 12499), "Active"),
+                ((12500, 100_000), "Very Active")
+            ])
         }
     }
 }
